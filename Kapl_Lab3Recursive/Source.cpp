2451a571f26002a0b677f0d602aef9ac85aa5805
rubyRain0 <den015326@gmail.com>
2022-12-25 15:44:06 +0300

    v2.99.99 ya ustal

--- Kapl_Lab3Recursive/Source.cpp
+++ Kapl_Lab3Recursive/Source.cpp
@@ -24,92 +24,66 @@ void printTree(const Tree& T)
         printTree(T->right);
     }
 }
-
 Tree copyTree(const Tree& node)
 {
     if (node == nullptr)
         return nullptr;
 
-    Tree T1 = new NODE(node->info);
+    Tree copy = new NODE(node->info);
+    copy->left = copyTree(node->left);
+    copy->right = copyTree(node->right);
+    return copy;
+}
+
+Tree derivativeTree(const Tree& node)
+{
+    if (node == nullptr)
+        return nullptr;
+
+    Tree tmp = new NODE(node->info);
     std::cout << node->info;
-    T1->left = copyTree(node->left);
-    T1->right = copyTree(node->right);
-    if (isOperator(T1->info) && T1->info != '*')
+    tmp->left = derivativeTree(node->left);
+    tmp->right = derivativeTree(node->right);
+    if (isOperator(tmp->info) && tmp->info != '*')
     {
-        if (!isOperator(T1->left->info) && !isOperator(T1->right->info)) //change basic tree u'+v'
-        {
-            T1->left->info = derivative(T1->left->info);
-            T1->right->info = derivative(T1->right->info);
-        }
-        //operator|operand cases
-        else if ((isOperator(T1->left->info) && !isOperator(T1->right->info)) ||
-            (!isOperator(T1->left->info) && isOperator(T1->right->info)))
-        {
-            if (isOperator(T1->left->info))
-                T1->right->info = derivative(T1->right->info);
-            else T1->left->info = derivative(T1->left->info);
-        }
+        if(!isOperator(tmp->left->info))
+            tmp->left->info = derivative(tmp->left->info);
+        if (!isOperator(tmp->right->info))
+            tmp->right->info = derivative(tmp->right->info);
     }
-    else if (T1->info == '*')
+    else if (tmp->info == '*')
     {
-        if (!isOperator(T1->left->info) && !isOperator(T1->right->info)) //change basic tree u'v+uv'
+        Tree t = new NODE('+');
+
+        Tree tmpCopy = copyTree(tmp);
+
+        t->left = tmp;
+        t->right = tmpCopy;
+        if(!isOperator(t->left->left->info))
+            t->left->left->info = derivative(t->left->left->info);
+        else
         {
-            Tree newT1ptr = new NODE('+');
-
-            newT1ptr->left = new NODE('*');
-            newT1ptr->right = new NODE('*');
-
-            newT1ptr->left->left = new NODE(derivative(T1->left->info));
-            newT1ptr->left->right = new NODE(T1->right->info);
-
-            newT1ptr->right->left = new NODE(T1->left->info);
-            newT1ptr->right->right = new NODE(derivative(T1->right->info));
-
-            T1 = newT1ptr;
+            Tree tll = copyTree(t->left->left);
+            t->left->left = derivativeTree(tll);
+            delete tll;
         }
-        //operator|operand cases
-        else if ((isOperator(T1->left->info) && !isOperator(T1->right->info)) ||
-            (!isOperator(T1->left->info) && isOperator(T1->right->info)))
+        if (!isOperator(t->right->right->info))
+            t->right->right->info = derivative(t->right->right->info);
+        else
         {
-            if (isOperator(T1->left->info))
-            {
-                Tree rememberT1LEFT = T1->left;
-                Tree rememberT1RIGHT = T1->right;
-                T1->info = '+';
-
-                T1->left = new NODE('*');
-                T1->left->left = copyTree(rememberT1LEFT); // LEFT PART DERIVATIVE. NEED TO GET (+)NEW NODE AND + V TO THE RIGHT
-                T1->left->right = rememberT1RIGHT;
-
-                T1->right = new NODE('*');
-                T1->right->left = rememberT1LEFT;
-                T1->right->right = new NODE(derivative(rememberT1RIGHT->info));
-            }
-            else if (isOperator(T1->right->info))
-            {
-                Tree rememberT1LEFT = T1->left;
-                Tree rememberT1RIGHT = T1->right;
-                T1->info = '+';
-
-                T1->left = new NODE('*');
-                T1->left->left = rememberT1RIGHT; // LEFT PART DERIVATIVE. NEED TO GET (+)NEW NODE AND + V TO THE RIGHT
-                T1->left->right = new NODE(derivative(rememberT1LEFT->info));
-
-                T1->right = new NODE('*');
-                T1->right->left = copyTree(rememberT1RIGHT);
-                T1->right->right = rememberT1LEFT;
-            }
+            Tree trr = copyTree(t->right->right);
+            t->right->right = derivativeTree(trr);
+            delete trr;
         }
+        tmp = t;      
     }
-
-    return T1;
-
+    return tmp;
 }
 
 bool task1(const Tree& T, Tree& T1, char derivativeVar)
 {
     bool result = true;
-    T1 = copyTree(T);
+    T1 = derivativeTree(T);
     if (!T1)
         result = false;
     return result;
